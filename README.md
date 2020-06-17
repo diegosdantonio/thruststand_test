5ec667db0529e3b429bd0974f8ff5d8fe5ae1c0f
diegosdantonio <diego.s.dantonio@gmail.com>
2020-06-16 22:17:26 -0400

    v1

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
 # Thruststant RCbenchmark test
 
-![RCbenchmark](media/1580_RCbenchmark.jpg | width=100)
-<img src="media/1580_RCbenchmark.jpg" alt="alt text" width="100">
+<img src="media/1580_RCbenchmark.jpg" alt="alt text" width="300">
 
 ### Test 1
 #### Materials
 
+* **Battery** Tattu 650 mAh 3CELLS
 * **Motor** EMAX RS1108 - 5200KV 
 * **Propeller** "EMAX avan Quad-Blade 2 inch
-* **Battery** Tattu 650 mAh 2CELLS
 * **ESC** AKK 20 A 4 en 1 BLHeli 2S–4S
 
-![](figures/Thrust_EMAXRS1108_EMAXavan2inch.png) ![](figures/Torque_EMAXRS1108_EMAXavan2inch.png)
+<img src="figures/Thrust_EMAXRS1108_EMAXavan2inch_3S.png" alt="alt text" width="300"> <img src="figures/Torque_EMAXRS1108_EMAXavan2inch_3S.png.png" alt="alt text" width="300">
 
 
-#### Test 2
+### Test 2
+#### Materials
 
+* **Battery** Tattu 650 mAh 2CELLS
+* **Motor** EMAX RS1108 - 5200KV 
+* **Propeller** "EMAX avan Quad-Blade 2 inch
+* **ESC** AKK 20 A 4 en 1 BLHeli 2S–4S
+
+<img src="figures/Thrust_EMAXRS1108_EMAXavan2inch.png" alt="alt text" width="300"> <img src="figures/Torque_EMAXRS1108_EMAXavan2inch.png" alt="alt text" width="300">
+
+#### Test 3
+
+* **Battery** Tattu 650 mAh 2CELLS
 * **Motor** BETA FPV 1105-6000KV
 * **Propeller** "EMAX avan Quad-Blade 2 inch
-* **Battery** Tattu 650 mAh 2CELLS
 * **ESC** AKK 20 A 4 en 1 BLHeli 2S–4S
 
-![](figures/Thrust_BETAFPV1105_EMAXavan2inch.png) ![](figures/Torque_BETAFPV1105_EMAXavan2inch.png)
+<img src="figures/Thrust_BETAFPV1105_EMAXavan2inch.png" alt="alt text" width="300"> <img src="figures/Torque_BETAFPV1105_EMAXavan2inch.png" alt="alt text" width="300">
 
--- main.py
+++ main.py
@@ -13,8 +13,19 @@ sys.setdefaultencoding('utf8')
 
 Motor_name = "EMAX RS1108 - 5200KV"
 Propeller_name = "EMAX avan Quad-Blade 2 inch"
-Batt = "Batt 2S"
-excel_file = 'datasets/StepsTest_2020-05-23_154840.csv'
+Batt = "Batt 3S"
+excel_file = 'datasets/StepsTest_2020-06-16_220513.csv'
+
+
+# Motor_name = "EMAX RS1108 - 5200KV"
+# Propeller_name = "EMAX avan Quad-Blade 2 inch"
+# Batt = "Batt 2S"
+# excel_file = 'datasets/StepsTest_2020-06-16_214944.csv'
+
+# Motor_name = "EMAX RS1108 - 5200KV"
+# Propeller_name = "EMAX avan Quad-Blade 2 inch"
+# Batt = "Batt 2S"
+# excel_file = 'datasets/StepsTest_2020-05-23_154840.csv'
 
 #Motor_name = "BETA FPV 1105-6000KV"
 #Propeller_name = "EMAX avan Quad-Blade 2 inch"
@@ -45,7 +56,7 @@ ax1.set_title('Thrust vs Motor Speed \n' + Batt+ ', '+ Motor_name + ' and ' + Pr
 ax1.grid(True)
 yy=(max(Y)-min(Y))*0.8
 color = 'tab:blue'
-ax1.text(10000, yy, "f(x) = {0:.4e}x^2 {1:+.4e}x {2:+.4e}".format(*fit), style='italic')
+ax1.text(X[0], yy, "f(x) = {0:.4e}x^2 {1:+.4e}x {2:+.4e}".format(*fit), style='italic')
 ax1.set_xlabel('Motor Speed (RPM)')
 ax1.set_ylabel('Thrust (gf)')
 ax1.plot(X, Y, color=color,marker="o", linestyle='None', label='Thrust (gf)')
@@ -83,7 +94,7 @@ ax2.grid(True)
 
 yy=(max(Y)-min(Y))*0.9
 color = 'tab:blue'
-ax2.text(10000, yy, "f(x) = {0:.4e}x^2 {1:+.4e}x {2:+.4e}".format(*fit), style='italic')
+ax2.text(X[0], yy, "f(x) = {0:.4e}x^2 {1:+.4e}x {2:+.4e}".format(*fit), style='italic')
 
 ax2.set_xlabel('Motor Speed (RPM)')
 ax2.set_ylabel(Column)
@@ -95,85 +106,6 @@ ax2.legend(loc='best')
 
 plt.show()
 
-####################################################
-################## Torque code #####################
-####################################################
-
-Column = "Propeller Mech. Efficiency (gf/W)"
-
-
-# Create vector X
-xmesh = np.linspace(min(data['Motor Electrical Speed (RPM)']), max(data['Motor Electrical Speed (RPM)']), 100)
-
-# Create vector for polyfit
-X = data.loc[:, "Motor Electrical Speed (RPM)"].values.reshape(-1, 1)
-Y = data.loc[:, Column].values.reshape(-1, 1)
-
-# Use poly fit and return polynomial equation
-fit = np.polyfit(data['Motor Electrical Speed (RPM)'], data[Column], 2)
-equation = np.poly1d(fit)
-print ("{0:.4e}x^2 {1:+.4e}) {2:+.4e}".format(*fit))
-
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.set_title('Propeller Mech. Efficiency vs Motor Speed \n'  + Batt+ ', ' + Motor_name + ' and ' + Propeller_name, fontsize=10, fontweight='bold')
-
-
-ax2.grid(True)
-
-yy=min(Y)
-color = 'tab:blue'
-ax2.text(10000, yy, "f(x) = {0:.4e}x^2 {1:+.4e}x {2:+.4e}".format(*fit), style='italic')
-
-ax2.set_xlabel('Motor Speed (RPM)')
-ax2.set_ylabel(Column)
-ax2.plot(X, Y, color=color,marker="o", linestyle='None', label = Column)
-ax2.plot(xmesh, equation(xmesh), '-r', label='Curve fit')
-
-ax2.legend(loc='best')
-
-
-plt.show()
-
-####################################################
-################## Torque code #####################
-####################################################
-
-Column = "Motor Efficiency (%)"
-
-
-# Create vector X
-xmesh = np.linspace(min(data['Motor Electrical Speed (RPM)']), max(data['Motor Electrical Speed (RPM)']), 100)
-
-# Create vector for polyfit
-X = data.loc[:, "Motor Electrical Speed (RPM)"].values.reshape(-1, 1)
-Y = data.loc[:, Column].values.reshape(-1, 1)
-
-# Use poly fit and return polynomial equation
-fit = np.polyfit(data['Motor Electrical Speed (RPM)'], data[Column], 2)
-equation = np.poly1d(fit)
-print ("{0:.4e}x^2 {1:+.4e}) {2:+.4e}".format(*fit))
-
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.set_title('Motor Efficiency vs Motor Speed \n'  + Batt+ ', ' + Motor_name + ' and ' + Propeller_name, fontsize=10, fontweight='bold')
-
-
-ax2.grid(True)
-
-yy=(max(Y)-min(Y))*0.9
-color = 'tab:blue'
-ax2.text(10000, yy, "f(x) = {0:.4e}x^2 {1:+.4e}x {2:+.4e}".format(*fit), style='italic')
-
-ax2.set_xlabel('Motor Speed (RPM)')
-ax2.set_ylabel(Column)
-ax2.plot(X, Y, color=color,marker="o", linestyle='None', label = Column)
-ax2.plot(xmesh, equation(xmesh), '-r', label='Curve fit')
-
-ax2.legend(loc='best')
-
-
-plt.show()
 
 
 #data.plot(kind='scatter',x='Motor Electrical Speed (RPM)',y='Thrust (gf)',color='red')
